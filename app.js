670cb135f341b25c0703dd6b2e11400aa0f1e7c2
Dmitry Gordienko <d.gordienko@gmail.com>
2015-06-23 00:32:30 +0300

    angular js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 var grhViewModule = angular.module("grhView",[]);
 grhViewModule.controller("GraphController",function($scope){
-
+ var date = Date.Now();
+ var dates = [];
+ for (var i = 0; i < 6; i++) {
+   dates.push(date.addDay(-i))
+ };
 });
