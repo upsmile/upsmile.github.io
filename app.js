ade066e2ae2bcac424fe275455fc1b91fda5928f
Dmitry Gordienko <d.gordienko@gmail.com>
2015-06-27 15:38:47 +0300

    add graphics controller

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var grhViewModule = angular.module("grhView",[]);
+
+grhViewModule.
 grhViewModule.controller("GraphController",function($scope){
- var date = Date.Now();
- var dates = [];
- for (var i = 0; i < 6; i++) {
-   dates.push(date.addDay(-i))
- };
+
+
 });
